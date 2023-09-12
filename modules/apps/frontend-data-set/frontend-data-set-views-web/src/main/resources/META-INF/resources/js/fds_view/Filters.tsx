05e7589afb9299b3c3f834d830ee7e0cd9f5825b
chestofwonders <miguelarroyoes@hotmail.com>
2023-09-12 12:08:18 +0200

    LPS-193106 Add date to fields formats

--- modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/fds_view/Filters.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/fds_view/Filters.tsx
@@ -38,6 +38,7 @@ enum EFilterType {
 }
 
 enum EFieldFormat {
+	DATE = 'date',
 	DATE_TIME = 'date-time',
 	STRING = 'string',
 }
@@ -207,7 +208,10 @@ function AddFDSFilterModalContent({
 		let displayType: string = '';
 		let url: string = '';
 
-		if (selectedField.format === EFieldFormat.DATE_TIME) {
+		if (
+			selectedField.format === EFieldFormat.DATE_TIME ||
+			selectedField.format === EFieldFormat.DATE
+		) {
 			url = API_URL.FDS_DATE_FILTERS;
 
 			body = {
@@ -846,7 +850,8 @@ function Filters({fdsView, fdsViewsURL, namespace}: IProps) {
 				(filterType === EFilterType.SELECTION &&
 					item.format === EFieldFormat.STRING) ||
 				(filterType === EFilterType.DATE_RANGE &&
-					item.format === EFieldFormat.DATE_TIME)
+					item.format === EFieldFormat.DATE_TIME) ||
+				item.format === EFieldFormat.DATE
 		);
 
 		if (!availableFields.length) {
