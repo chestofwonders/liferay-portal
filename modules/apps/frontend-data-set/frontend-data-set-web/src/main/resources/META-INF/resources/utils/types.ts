26dc455d22812eddb553aef5e5814f104df38f13
Kevin Tan <kevin.tan@liferay.com>
2025-09-18 12:42:12 -0700

    LPD-65514 Add type to items actions type property

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/utils/types.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/utils/types.ts
@@ -145,7 +145,12 @@ export interface IItemsActions {
 		| 'modal-permissions'
 		| 'sidePanel'
 		| 'event';
-	type?: string;
+	type?: EItemActionsType;
+}
+
+export enum EItemActionsType {
+	GROUP = 'group',
+	ITEM = 'item',
 }
 
 export interface IItemActionsData {
