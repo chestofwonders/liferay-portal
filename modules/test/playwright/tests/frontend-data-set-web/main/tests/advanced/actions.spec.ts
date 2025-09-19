306353f86426171cf94303b68afadaafbacbb561
Kevin Tan <kevin.tan@liferay.com>
2025-09-19 11:30:57 -0700

    LPD-65514 playwright: Perform same tests on the Items Actions Groups tab

--- modules/test/playwright/tests/frontend-data-set-web/main/tests/advanced/actions.spec.ts
+++ modules/test/playwright/tests/frontend-data-set-web/main/tests/advanced/actions.spec.ts
@@ -23,266 +23,314 @@ const test = mergeTests(
 	loginTest()
 );
 
-test.beforeEach(async ({fdsSamplePage, page, site}) => {
-	await fdsSamplePage.setupFDSSampleWidget({site});
+const TAB_NAME = {
+	ADVANCED: 'Advanced',
+	ITEMS_ACTIONS_GROUPS: 'Items Actions Groups',
+};
 
-	await fdsSamplePage.selectTab('Advanced');
+const TABS = [
+	{
+		actionsCount: 14,
+		name: TAB_NAME.ADVANCED,
+	},
+	{
+		actionsCount: 15,
+		name: TAB_NAME.ITEMS_ACTIONS_GROUPS,
+	},
+];
 
-	await waitForFDS({page, visualizationMode: EFDSVisualizationMode.TABLE});
-});
+for (const tab of TABS) {
+	test.describe(`Test Actions On Tab: ${tab.name}`, () => {
+		test.beforeEach(async ({fdsSamplePage, page, site}) => {
+			await fdsSamplePage.setupFDSSampleWidget({site});
 
-test('Behavior of item actions', async ({fdsSamplePage, page}) => {
-	const asyncConnectionRefused = 'Async Connection Refused';
-	const asyncResourceNotFound = 'Async Resource Not Found';
-	const asyncSuccess = 'Async Success';
-	const sampleView = 'Sample View';
-	const sidePanelActionLabelWithActionTitle = 'Side Panel With Action Title';
-	const sidePanelActionLabelWithContentTitle =
-		'Side Panel With Content Title';
-	const sidePanelActionLabelWithActionTitleContentTitle =
-		'Side Panel With Action and Content Title';
-	const sidePanelActionLabelWithoutTitle = 'Side Panel With No Title';
-	const sidePanelActionTitle = 'Side Panel Title Provided by Action';
-	const sidePanelContentTitle = 'Side Panel Title Provided by Page';
+			await fdsSamplePage.selectTab(tab.name);
 
-	await test.step('Check that the Item Actions dropdown is present in table row', async () => {
-		const tableItemActionButton =
-			fdsSamplePage.table.itemActionButtons.first();
-
-		await expect(tableItemActionButton).toBeVisible();
-
-		const dropdownMenu = await fdsSamplePage.getDropdownId(
-			tableItemActionButton
-		);
-
-		await expect(dropdownMenu.getByRole('menuitem')).toHaveCount(14);
-
-		await page.keyboard.press('Escape');
-	});
-
-	await test.step('Check that the Item Actions dropdown displays icons for Table, List, and Cards views', async () => {
-		await test.step('Check Table view actions dropdown items has icons', async () => {
-			const tableItemActionButton =
-				fdsSamplePage.table.itemActionButtons.first();
-
-			await expect(tableItemActionButton).toBeVisible();
-
-			await fdsSamplePage.checkDropdownMenuIconsAreVisible(
-				tableItemActionButton
-			);
-		});
-
-		await test.step('Check List view actions dropdown items has icons', async () => {
-			await fdsSamplePage.changeVisualizationMode({
-				page,
-				visualizationMode: EFDSVisualizationMode.LIST,
-			});
-
-			const listItemActionButton =
-				fdsSamplePage.list.itemActionButtons.first();
-
-			await expect(listItemActionButton).toBeVisible();
-
-			await fdsSamplePage.checkDropdownMenuIconsAreVisible(
-				listItemActionButton
-			);
-		});
-
-		await test.step('Check Cards view action dropdown items has icons', async () => {
-			await fdsSamplePage.changeVisualizationMode({
-				page,
-				visualizationMode: EFDSVisualizationMode.CARDS,
-			});
-
-			const cardItemActionButton =
-				fdsSamplePage.cards.itemActionButtons.first();
-
-			await expect(cardItemActionButton).toBeVisible();
-
-			await fdsSamplePage.checkDropdownMenuIconsAreVisible(
-				cardItemActionButton
-			);
-		});
-
-		await test.step('Switch back to Table view', async () => {
-			await fdsSamplePage.changeVisualizationMode({
+			await waitForFDS({
 				page,
 				visualizationMode: EFDSVisualizationMode.TABLE,
 			});
 		});
-	});
 
-	await test.step('Side Panel action opens a side panel with content title', async () => {
-		await fdsSamplePage.clickItemAction(
-			sidePanelActionLabelWithContentTitle
-		);
+		test('Behavior of item actions', async ({fdsSamplePage, page}) => {
+			const asyncConnectionRefused = 'Async Connection Refused';
+			const asyncResourceNotFound = 'Async Resource Not Found';
+			const asyncSuccess = 'Async Success';
+			const sampleView = 'Sample View';
+			const sidePanelActionLabelWithActionTitle =
+				'Side Panel With Action Title';
+			const sidePanelActionLabelWithContentTitle =
+				'Side Panel With Content Title';
+			const sidePanelActionLabelWithActionTitleContentTitle =
+				'Side Panel With Action and Content Title';
+			const sidePanelActionLabelWithoutTitle = 'Side Panel With No Title';
+			const sidePanelActionTitle = 'Side Panel Title Provided by Action';
+			const sidePanelContentTitle = 'Side Panel Title Provided by Page';
 
-		await expect(fdsSamplePage.sidePanel).toBeInViewport();
+			await test.step('Check that the Item Actions dropdown is present in table row', async () => {
+				const tableItemActionButton =
+					fdsSamplePage.table.itemActionButtons.first();
 
-		const frame = fdsSamplePage.sidePanelFrame;
+				await expect(tableItemActionButton).toBeVisible();
 
-		await frame.getByText(sidePanelContentTitle).waitFor();
+				const dropdownMenu = await fdsSamplePage.getDropdownId(
+					tableItemActionButton
+				);
 
-		await expect(frame.getByText(sidePanelContentTitle)).toHaveCount(1);
+				await expect(dropdownMenu.getByRole('menuitem')).toHaveCount(
+					tab.actionsCount
+				);
 
-		await expect(
-			frame.getByText('This is a side panel with a title.')
-		).toBeVisible();
+				await page.keyboard.press('Escape');
+			});
 
-		await page.keyboard.press('Escape');
+			await test.step('Check that the Item Actions dropdown displays icons for Table, List, and Cards views', async () => {
+				await test.step('Check Table view actions dropdown items has icons', async () => {
+					const tableItemActionButton =
+						fdsSamplePage.table.itemActionButtons.first();
 
-		await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
-	});
+					await expect(tableItemActionButton).toBeVisible();
 
-	await test.step('Side Panel action opens a side panel with action title', async () => {
-		await fdsSamplePage.clickItemAction(
-			sidePanelActionLabelWithActionTitle
-		);
-
-		await expect(fdsSamplePage.sidePanel).toBeInViewport();
-
-		await page.getByText(sidePanelActionTitle).waitFor();
-
-		await expect(page.getByText(sidePanelActionTitle)).toHaveCount(1);
-
-		const frame = fdsSamplePage.sidePanelFrame;
-
-		await expect(
-			frame.locator('.side-panel-iframe-header')
-		).not.toBeInViewport();
-
-		await expect(
-			frame.getByText('This is a side panel without a title.')
-		).toBeVisible();
-
-		await page.keyboard.press('Escape');
-
-		await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
-	});
-
-	await test.step('Side Panel action opens a side panel with duplicated title', async () => {
-		await fdsSamplePage.clickItemAction(
-			sidePanelActionLabelWithActionTitleContentTitle
-		);
-
-		await expect(fdsSamplePage.sidePanel).toBeInViewport();
-
-		await page.getByText(sidePanelActionTitle).waitFor();
-
-		await expect(page.getByText(sidePanelActionTitle)).toHaveCount(1);
-
-		const frame = fdsSamplePage.sidePanelFrame;
-
-		await expect(
-			frame.locator('.side-panel-iframe-header')
-		).toBeInViewport();
-		await frame.getByText(sidePanelContentTitle).waitFor();
-
-		await expect(frame.getByText(sidePanelContentTitle)).toHaveCount(1);
-
-		await page.keyboard.press('Escape');
-
-		await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
-	});
-
-	await test.step('Side Panel action opens a side panel without title', async () => {
-		await fdsSamplePage.clickItemAction(sidePanelActionLabelWithoutTitle);
-
-		await expect(fdsSamplePage.sidePanel).toBeInViewport();
-
-		await expect(page.locator('.fds-side-panel-title')).toBeInViewport();
-		const panelTitle = await page
-			.locator('.fds-side-panel-title')
-			.allInnerTexts();
-
-		expect(panelTitle).toEqual(['']);
-
-		const frame = fdsSamplePage.sidePanelFrame;
-
-		await expect(
-			frame.locator('.side-panel-iframe-header')
-		).not.toBeInViewport();
-
-		await expect(
-			frame.getByText('This is a side panel without a title.')
-		).toBeVisible();
-
-		await page.keyboard.press('Escape');
-
-		await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
-	});
-
-	await test.step('Sample view action opens an alert message', async () => {
-		let dialogMessage = '';
-
-		page.on('dialog', async (dialog) => {
-			dialogMessage = dialog.message();
-			await dialog.accept();
-		});
-
-		for (const visualizationMode of Object.values(EFDSVisualizationMode)) {
-			await test.step(`Sample view action receives the list of items, ${visualizationMode}`, async () => {
-				await fdsSamplePage.changeVisualizationMode({
-					page,
-					visualizationMode,
+					await fdsSamplePage.checkDropdownMenuIconsAreVisible(
+						tableItemActionButton
+					);
 				});
 
-				await fdsSamplePage.clickItemAction(sampleView);
-				expect(dialogMessage).toContain('Hello Sample1!');
-				expect(dialogMessage).toContain('element #1');
+				await test.step('Check List view actions dropdown items has icons', async () => {
+					await fdsSamplePage.changeVisualizationMode({
+						page,
+						visualizationMode: EFDSVisualizationMode.LIST,
+					});
 
-				await fdsSamplePage.clickItemAction(sampleView, 19);
-				expect(dialogMessage).toContain('Hello Sample32!');
-				expect(dialogMessage).toContain('element #20');
+					const listItemActionButton =
+						fdsSamplePage.list.itemActionButtons.first();
+
+					await expect(listItemActionButton).toBeVisible();
+
+					await fdsSamplePage.checkDropdownMenuIconsAreVisible(
+						listItemActionButton
+					);
+				});
+
+				await test.step('Check Cards view action dropdown items has icons', async () => {
+					await fdsSamplePage.changeVisualizationMode({
+						page,
+						visualizationMode: EFDSVisualizationMode.CARDS,
+					});
+
+					const cardItemActionButton =
+						fdsSamplePage.cards.itemActionButtons.first();
+
+					await expect(cardItemActionButton).toBeVisible();
+
+					await fdsSamplePage.checkDropdownMenuIconsAreVisible(
+						cardItemActionButton
+					);
+				});
+
+				await test.step('Switch back to Table view', async () => {
+					await fdsSamplePage.changeVisualizationMode({
+						page,
+						visualizationMode: EFDSVisualizationMode.TABLE,
+					});
+				});
 			});
-		}
 
-		await fdsSamplePage.changeVisualizationMode({
-			page,
-			visualizationMode: EFDSVisualizationMode.TABLE,
+			await test.step('Side Panel action opens a side panel with content title', async () => {
+				await fdsSamplePage.clickItemAction(
+					sidePanelActionLabelWithContentTitle
+				);
+
+				await expect(fdsSamplePage.sidePanel).toBeInViewport();
+
+				const frame = fdsSamplePage.sidePanelFrame;
+
+				await frame.getByText(sidePanelContentTitle).waitFor();
+
+				await expect(
+					frame.getByText(sidePanelContentTitle)
+				).toHaveCount(1);
+
+				await expect(
+					frame.getByText('This is a side panel with a title.')
+				).toBeVisible();
+
+				await page.keyboard.press('Escape');
+
+				await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
+			});
+
+			await test.step('Side Panel action opens a side panel with action title', async () => {
+				await fdsSamplePage.clickItemAction(
+					sidePanelActionLabelWithActionTitle
+				);
+
+				await expect(fdsSamplePage.sidePanel).toBeInViewport();
+
+				await page.getByText(sidePanelActionTitle).waitFor();
+
+				await expect(page.getByText(sidePanelActionTitle)).toHaveCount(
+					1
+				);
+
+				const frame = fdsSamplePage.sidePanelFrame;
+
+				await expect(
+					frame.locator('.side-panel-iframe-header')
+				).not.toBeInViewport();
+
+				await expect(
+					frame.getByText('This is a side panel without a title.')
+				).toBeVisible();
+
+				await page.keyboard.press('Escape');
+
+				await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
+			});
+
+			await test.step('Side Panel action opens a side panel with duplicated title', async () => {
+				await fdsSamplePage.clickItemAction(
+					sidePanelActionLabelWithActionTitleContentTitle
+				);
+
+				await expect(fdsSamplePage.sidePanel).toBeInViewport();
+
+				await page.getByText(sidePanelActionTitle).waitFor();
+
+				await expect(page.getByText(sidePanelActionTitle)).toHaveCount(
+					1
+				);
+
+				const frame = fdsSamplePage.sidePanelFrame;
+
+				await expect(
+					frame.locator('.side-panel-iframe-header')
+				).toBeInViewport();
+				await frame.getByText(sidePanelContentTitle).waitFor();
+
+				await expect(
+					frame.getByText(sidePanelContentTitle)
+				).toHaveCount(1);
+
+				await page.keyboard.press('Escape');
+
+				await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
+			});
+
+			await test.step('Side Panel action opens a side panel without title', async () => {
+				await fdsSamplePage.clickItemAction(
+					sidePanelActionLabelWithoutTitle
+				);
+
+				await expect(fdsSamplePage.sidePanel).toBeInViewport();
+
+				await expect(
+					page.locator('.fds-side-panel-title')
+				).toBeInViewport();
+				const panelTitle = await page
+					.locator('.fds-side-panel-title')
+					.allInnerTexts();
+
+				expect(panelTitle).toEqual(['']);
+
+				const frame = fdsSamplePage.sidePanelFrame;
+
+				await expect(
+					frame.locator('.side-panel-iframe-header')
+				).not.toBeInViewport();
+
+				await expect(
+					frame.getByText('This is a side panel without a title.')
+				).toBeVisible();
+
+				await page.keyboard.press('Escape');
+
+				await expect(fdsSamplePage.sidePanel).toHaveClass(/is-hidden/);
+			});
+
+			await test.step('Sample view action opens an alert message', async () => {
+				let dialogMessage = '';
+
+				page.on('dialog', async (dialog) => {
+					dialogMessage = dialog.message();
+					await dialog.accept();
+				});
+
+				for (const visualizationMode of Object.values(
+					EFDSVisualizationMode
+				)) {
+					await test.step(`Sample view action receives the list of items, ${visualizationMode}`, async () => {
+						await fdsSamplePage.changeVisualizationMode({
+							page,
+							visualizationMode,
+						});
+
+						await fdsSamplePage.clickItemAction(sampleView);
+						expect(dialogMessage).toContain('Hello Sample1!');
+						expect(dialogMessage).toContain('element #1');
+
+						await fdsSamplePage.clickItemAction(sampleView, 19);
+						expect(dialogMessage).toContain('Hello Sample32!');
+						expect(dialogMessage).toContain('element #20');
+					});
+				}
+
+				await fdsSamplePage.changeVisualizationMode({
+					page,
+					visualizationMode: EFDSVisualizationMode.TABLE,
+				});
+			});
+
+			await test.step('Async connection refused action opens an unexpected error alert toast', async () => {
+				await fdsSamplePage.clickItemAction(asyncConnectionRefused);
+
+				await waitForAlert(
+					page,
+					'Error:An unexpected error occurred.',
+					{
+						type: 'danger',
+					}
+				);
+			});
+
+			await test.step('Async resource not found action opens an unexpected error alert toast', async () => {
+				await fdsSamplePage.clickItemAction(asyncResourceNotFound);
+
+				await waitForAlert(
+					page,
+					'Error:An unexpected error occurred.',
+					{
+						type: 'danger',
+					}
+				);
+			});
+
+			await test.step('Async success action opens a success alert toast', async () => {
+				await fdsSamplePage.clickItemAction(asyncSuccess);
+
+				await waitForAlert(page);
+			});
+
+			await test.step('Check that Sample Delete action has custom className applied', async () => {
+				const tableItemActionButton =
+					fdsSamplePage.table.itemActionButtons.first();
+
+				await expect(tableItemActionButton).toBeVisible();
+
+				const dropdownMenu = await fdsSamplePage.getDropdownId(
+					tableItemActionButton
+				);
+
+				const sampleDeleteActionItem = dropdownMenu
+					.getByRole('menuitem')
+					.filter({hasText: 'Sample Delete'});
+
+				await expect(sampleDeleteActionItem).toBeVisible();
+
+				await expect(sampleDeleteActionItem).toHaveClass(/text-danger/);
+
+				await page.keyboard.press('Escape');
+			});
 		});
 	});
-
-	await test.step('Async connection refused action opens an unexpected error alert toast', async () => {
-		await fdsSamplePage.clickItemAction(asyncConnectionRefused);
-
-		await waitForAlert(page, 'Error:An unexpected error occurred.', {
-			type: 'danger',
-		});
-	});
-
-	await test.step('Async resource not found action opens an unexpected error alert toast', async () => {
-		await fdsSamplePage.clickItemAction(asyncResourceNotFound);
-
-		await waitForAlert(page, 'Error:An unexpected error occurred.', {
-			type: 'danger',
-		});
-	});
-
-	await test.step('Async success action opens a success alert toast', async () => {
-		await fdsSamplePage.clickItemAction(asyncSuccess);
-
-		await waitForAlert(page);
-	});
-
-	await test.step('Check that Sample Delete action has custom className applied', async () => {
-		const tableItemActionButton =
-			fdsSamplePage.table.itemActionButtons.first();
-
-		await expect(tableItemActionButton).toBeVisible();
-
-		const dropdownMenu = await fdsSamplePage.getDropdownId(
-			tableItemActionButton
-		);
-
-		const sampleDeleteActionItem = dropdownMenu
-			.getByRole('menuitem')
-			.filter({hasText: 'Sample Delete'});
-
-		await expect(sampleDeleteActionItem).toBeVisible();
-
-		await expect(sampleDeleteActionItem).toHaveClass(/text-danger/);
-
-		await page.keyboard.press('Escape');
-	});
-});
+}
