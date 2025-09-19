b7efbbd201f034bf792d86ea2ad91ffa77067250
Kevin Tan <kevin.tan@liferay.com>
2025-09-19 10:52:24 -0700

    LPD-65514 frontend-data-set-sample-web: Add a separate sample tab "Items Actions Group"
    This allows testing for both items using groups and not using groups. Items not using groups is, at the moment, more important since it's how all of FDS itemsGroups are currently being used. This separate tab allows testing for the same actions but placed in a group.

--- modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/resources/META-INF/resources/js/AdvancedPropsTransformer.ts
+++ modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/resources/META-INF/resources/js/AdvancedPropsTransformer.ts
@@ -21,7 +21,7 @@ import type {
 } from '@liferay/frontend-data-set-web';
 
 export default function propsTransformer({
-	additionalProps: {greeting},
+	additionalProps: {enableItemsActionsGroups, greeting},
 	itemsActions,
 	selectedItemsKey,
 	...otherProps
@@ -127,56 +127,53 @@ export default function propsTransformer({
 		},
 		fileDropSettings,
 		infoPanelComponent: SampleInfoPanel,
-
-		// Ideally, placing items in groups should happen in the definition of
-		// the actions (AdvancedFDSItemsActions.java) but this is done here in
-		// the meantime while the java API is being decided on.
-
-		itemsActions: [
-			{
-				items: itemActionsWithStyling,
-				separator: true,
-				type: 'group',
-			},
-			{
-				items: [
+		itemsActions: enableItemsActionsGroups
+			? [
 					{
-						data: {
-							id: 'emptyGroupTest',
-							permissionKey: 'nonexistentPermissionKey',
-						},
-						icon: 'hidden',
-						label: 'Empty Group Test',
-					},
-				],
-				separator: true,
-				type: 'group',
-			},
-			{
-				items: [
-					{
-						data: {
-							id: 'groupItem',
-						},
-						icon: 'separator',
-						label: 'Group Item',
-						onClick: () => {
-							alert('You clicked on an item in a group');
-						},
+						items: itemActionsWithStyling,
+						separator: true,
+						type: 'group',
 					},
 					{
-						data: {
-							id: 'groupPermissionTest',
-							permissionKey: 'nonexistentPermissionKey',
-						},
-						icon: 'hidden',
-						label: 'Group Permission Test',
+						items: [
+							{
+								data: {
+									id: 'emptyGroupTest',
+									permissionKey: 'nonexistentPermissionKey',
+								},
+								icon: 'hidden',
+								label: 'Empty Group Test',
+							},
+						],
+						separator: true,
+						type: 'group',
 					},
-				],
-				separator: true,
-				type: 'group',
-			},
-		],
+					{
+						items: [
+							{
+								data: {
+									id: 'groupItem',
+								},
+								icon: 'separator',
+								label: 'Group Item',
+								onClick: () => {
+									alert('You clicked on an item in a group');
+								},
+							},
+							{
+								data: {
+									id: 'groupPermissionTest',
+									permissionKey: 'nonexistentPermissionKey',
+								},
+								icon: 'hidden',
+								label: 'Group Permission Test',
+							},
+						],
+						separator: true,
+						type: 'group',
+					},
+				]
+			: itemActionsWithStyling,
 		onActionDropdownItemClick({
 			action,
 			itemData,
